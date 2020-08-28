fc6838fec68803cb18807a9e068f3e915c9f2c84
luweishuang <pifuchan@gmail.com>
2020-08-28 09:58:02 +0800

    reading code

--- sentence_transformers/losses/CosineSimilarityLoss.py
+++ sentence_transformers/losses/CosineSimilarityLoss.py
@@ -31,12 +31,12 @@ class CosineSimilarityLoss(nn.Module):
     def __init__(self, model: SentenceTransformer, loss_fct = nn.MSELoss(), cos_score_transformation=nn.Identity()):
         super(CosineSimilarityLoss, self).__init__()
         self.model = model
-        self.loss_fct = loss_fct
-        self.cos_score_transformation = cos_score_transformation
+        self.loss_fct = loss_fct           # nn.MSELoss() ==> 均方损失函数
+        self.cos_score_transformation = cos_score_transformation   # nn.Identity() ==> A placeholder identity operator
 
 
     def forward(self, sentence_features: Iterable[Dict[str, Tensor]], labels: Tensor):
         embeddings = [self.model(sentence_feature)['sentence_embedding'] for sentence_feature in sentence_features]
-        output = self.cos_score_transformation(torch.cosine_similarity(embeddings[0], embeddings[1]))
+        output = self.cos_score_transformation(torch.cosine_similarity(embeddings[0], embeddings[1]))   # torch.cosine_similarity ==> calculate cos distance
         return self.loss_fct(output, labels.view(-1))
 
